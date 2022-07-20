b61aa505779e249e84bce7875deab27d12e41d58
MrRojito-Dev <cancinojuaco@gmail.com>
2022-07-20 02:51:12 -0400

    little change

--- src/routes/perfil.js
+++ src/routes/perfil.js
@@ -4,6 +4,23 @@ const CheckAuth = require('../utils/auth');
 const utils = require('../utils/utils');
 const fetch = require("node-fetch");
 
+router.get("/:userID", async (req, res) => {
+  const user = await req.client.users.fetch(req.user ? req.user.id : null).catch(() => false);
+  const userProfile = await fetch(`${process.env.FETCH}/perfil/${req.params.userID}`, {
+      method: "GET",
+      headers: {
+        "pass": `${process.env.ACCESS}`
+      }
+  });
+
+  res.render('perfiles/index', {
+      title: "Piña Bot",
+      user,
+      profile: userProfile.ok ? await userProfile.json() : null,
+      humanize: utils.humanize
+  });
+});
+
 router.use(CheckAuth, async (req, res, next) => next());
 
 router.get("/", async (req, res) => {
@@ -19,23 +36,6 @@ router.get("/", async (req, res) => {
         title: "Piña Bot",
         user,
         profile: await userProfile.json(),
-        humanize: utils.humanize
-    });
-});
-
-router.get("/:userID", async (req, res) => {
-    const user = await req.client.users.fetch(req.user ? req.user.id : null).catch(() => false);
-    const userProfile = await fetch(`${process.env.FETCH}/perfil/${req.params.userID}`, {
-        method: "GET",
-        headers: {
-          "pass": `${process.env.ACCESS}`
-        }
-    });
-
-    res.render('perfiles/index', {
-        title: "Piña Bot",
-        user,
-        profile: userProfile.ok ? await userProfile.json() : null,
         humanize: utils.humanize
     });
 });
